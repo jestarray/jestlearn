d11d25c1daca3b5430c61c385c824bb52c9f4f4e
jestarray <34615798+jestarray@users.noreply.github.com>
2021-10-31 10:35:11 -0700

    trying to get giscus comments workin

--- src/Discussions.svelte
+++ src/Discussions.svelte
@@ -20,21 +20,22 @@
 </code>
 &lt;/details&gt;
 </pre>
+
+  <script
+    src="https://giscus.app/client.js"
+    data-repo="jestarray/howtocode"
+    data-repo-id="MDEwOlJlcG9zaXRvcnk0MDM3MTI1NTA="
+    data-category="Assignment Submissions"
+    data-category-id="DIC_kwDOGBAqJs4B_l8z"
+    data-mapping="og:title"
+    data-reactions-enabled="1"
+    data-emit-metadata="0"
+    data-theme="light"
+    data-lang="en"
+    crossorigin="anonymous"
+    async>
+  </script>
   <div class="giscus">
-    <script
-      src="https://giscus.app/client.js"
-      data-repo="jestarray/howtocode"
-      data-repo-id="MDEwOlJlcG9zaXRvcnk0MDM3MTI1NTA="
-      data-category="Assignment Submissions"
-      data-category-id="DIC_kwDOGBAqJs4B_l8z"
-      data-mapping="og:title"
-      data-reactions-enabled="1"
-      data-emit-metadata="0"
-      data-theme="light"
-      data-lang="en"
-      crossorigin="anonymous"
-      async>
-    </script>
     <div class="giscus-frame" />
   </div>
 </div>
